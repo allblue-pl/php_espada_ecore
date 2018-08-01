56f302638013b660007ca1e7dff64da2066b5cf7
SfTd <j.zolcik@allblue.pl>
2018-08-01 10:40:07 +0200

    Added HTTPRequest.

---
+++ HttpRequest/classes/CHttpRequest.php
@@ -0,0 +1,81 @@
+<?php namespace EC\HttpRequest;
+defined('_ESPADA') or die(NO_ACCESS);
+
+require(__DIR__ . '/../composer/vendor/autoload.php');
+
+use E, EC;
+
+class CHttpRequest
+{
+
+    private $auth = null;
+
+    public function __construct()
+    {
+        $this->client = new \GuzzleHttp\Client();
+    }
+
+    public function delete($url)
+    {
+        return $this->request('DELETE', $url, [
+            'http_errors' => false,
+        ]);
+    }
+
+    public function delete_JSON($url, array $json = [])
+    {
+        return $this->request('DELETE', $url, [
+            'json' => $json,
+            'http_errors' => false,
+        ]);
+    }
+
+    public function get($url, $fields = [])
+    {
+        return $this->request('GET', $url);
+    }
+
+    public function get_JSON($url)
+    {
+        return $this->request('GET', $url, [
+            'http_errors' => false,
+        ]);
+    }
+
+    public function post($url, $fields = [])
+    {
+        return $this->request('POST', $url, [
+            'form_params' => $fields,
+        ]);
+    }
+
+    public function post_JSON($url, array $json)
+    {
+        return $this->request('POST', $url, [
+            'json' => $json,
+            'http_errors' => false,
+        ]);
+    }
+
+    public function put_JSON($url, array $json)
+    {
+        return $this->request('PUT', $url, [
+            'json' => $json,
+            'http_errors' => false,
+        ]);
+    }
+
+    public function request($type, $url, $extra = [])
+    {
+        if ($this->auth !== null)
+            $extra['auth'] = $this->auth;
+
+        return $this->client->request($type, $url, $extra);
+    }
+
+    public function setAuth(string $user, string $password)
+    {
+        $this->auth = [ $user, $password ];
+    }
+
+}
