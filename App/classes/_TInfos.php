5be549bf7316be500e571c13aa3c2b598cf5f473
SfTd <j.zolcik@allblue.pl>
2019-03-15 16:00:30 +0100

    Development.

---
+++ App/classes/_TInfos.php
@@ -0,0 +1,22 @@
+<?php namespace EC\App;
+defined('_ESPADA') or die(NO_ACCESS);
+
+use E, EC,
+    EC\Database;
+
+class _TInfos extends Database\TTable
+{
+
+    public function __construct(EC\MDatabase $db, $tablePrefix)
+    {
+        parent::__construct($db, 'App_Infos', $tablePrefix);
+
+        $this->setColumns([
+            'Id' => new Database\FInt(true),
+            'User_Id' => new Database\FInt(true),
+            'AuthenticationHash' => new Database\FVarchar(true, 256),
+            'TablesIds' => new Database\FText(true, 'medium'),
+        ]);
+    }
+
+}
