aa097add61986012503ae548303512b59e3b1a1c
Jakub Zolcik <j.zolcik@allblue.pl>
2021-08-30 17:55:51 +0200

    Development.

--- CookiesPolicy/js-libs/e-cookies-policy/index.js
+++ CookiesPolicy/js-libs/e-cookies-policy/index.js
@@ -2,31 +2,56 @@
 'use strict';
 
 const
-    abCookies = require('ab-cookies')
+    abCookies = require('ab-cookies'),
+    js0 = require('js0')
 ;
 
-
-export function init()
+class eCookiesPolicy_Class
 {
-    if (abCookies.get('eCookiesPolicy_Displayed'))
-        return;
 
-    let modal = new bootstrap.Modal(document.getElementById('ECookiesPolicy_Modal'));
-    
-    let close = () => {
-        abCookies.set('eCookiesPolicy_Displayed', true);
-        modal.hide();
+    constructor()
+    {
+        this._listeners_OnClose = [];
     }
 
-    document.getElementById('eCookiesPolicy_Close').addEventListener('click', (evt) => {
-        evt.preventDefault();
-        close();
-    });
+    addListener_OnClose(listenerFn)
+    {
+        js0.args(arguments, 'function');
 
-    document.getElementById('eCookiesPolicy_Agree').addEventListener('click', (evt) => {
-        evt.preventDefault();
-        close();
-    });
+        this._listeners_OnClose.push(listenerFn);
+    }
+
+    init()
+    {
+        if (abCookies.get('eCookiesPolicy_Displayed')) {
+            for (let listenerFn of this._listeners_OnClose)
+                listenerFn();
+
+            return;
+        }
     
-    modal.show();
+        let modal = new bootstrap.Modal(document.getElementById('ECookiesPolicy_Modal'));
+        
+        let close = () => {
+            abCookies.set('eCookiesPolicy_Displayed', true);
+            modal.hide();
+
+            for (let listenerFn of this._listeners_OnClose)
+                listenerFn();
+        }
+    
+        document.getElementById('eCookiesPolicy_Close').addEventListener('click', (evt) => {
+            evt.preventDefault();
+            close();
+        });
+    
+        document.getElementById('eCookiesPolicy_Agree').addEventListener('click', (evt) => {
+            evt.preventDefault();
+            close();
+        });
+        
+        modal.show();
+    }
+
 }
+export default eCookiesPolicy = new eCookiesPolicy_Class();
