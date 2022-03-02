77ae40ee5f0f9c19bfacdfe7411c2acf4d0c25d1
AllBlue Monkey <j.zolcik@allblue.pl>
2022-03-02 15:44:44 +0100

    Development.

--- PDF/classes/CReader.php
+++ PDF/classes/CReader.php
@@ -2,6 +2,7 @@
 defined('_ESPADA') or die(NO_ACCESS);
 
 include(__DIR__ . '/../3rdparty/pdfparser/vendor/autoload.php');
+include(__DIR__ . '/../3rdparty/tc-lib-pdf-parser/vendor/autoload.php');
 
 use E, EC;
 
@@ -24,8 +25,24 @@ class CReader
         foreach ($pages as $page) {
             $page_fields = $page->getTextArray();
             foreach ($page_fields as $field) {
-                // echo "{$field}\r\n";
+                // echo EC\Strings\HEncoding::Convert($field, 'utf-8', 'windows-1250') . "\r\n";
                 $fields[] = $field;
+            }
+        }
+
+        return $fields;
+    }
+
+    public function getFields_Hex()
+    {
+        $fields = [];
+
+        $pages = $this->pdf->getPages();
+        foreach ($pages as $page) {
+            $page_fields = $page->extractRawData();
+            foreach ($page_fields as $field) {
+                if ($field['o'] === 'Tj')
+                    $fields[] = $field['c'];
             }
         }
 
@@ -46,5 +63,25 @@ class CReader
 
         return true;
     }
+
+    // public function getFields()
+    // {
+    //     var_dump($this->pdf);
+    //     die;
+    // }
+
+    // public function read($filePath)
+    // {
+    //     $pdf_Raw = file_get_contents($filePath);
+    //     if ($pdf_Raw === false)
+    //         return false;
+
+    //     $this->parser = new \Com\Tecnick\Pdf\Parser\Parser([ 
+    //         'ignore_filter_errors' => true, 
+    //     ]);
+    //     $this->pdf = $this->parser->parse($pdf_Raw);
+
+    //     return true;
+    // }
 
 }
