fe70f1c5c264cf2ed83b21efa9ea90fed95e2b62
AllBlue Monkey <j.zolcik@allblue.pl>
2022-11-09 16:07:22 +0100

    Development.

--- Upload/classes/HUpload.php
+++ Upload/classes/HUpload.php
@@ -8,24 +8,22 @@ class HUpload
 
     static public function Validate($file, $info = [], &$error = null)
     {
-        if ($file !== null) {
-            if ($file['error'] !== UPLOAD_ERR_OK) {
-                if ($file['error'] === UPLOAD_ERR_INI_SIZE ||
-                        $file['error'] === UPLOAD_ERR_FORM_SIZE) {
-                    $error = EC\HText::_('Upload:Errors_FileTooBig');
-                    return false;
-                }
-
-                $error = EC\HText::_('Upload:Errors_CannotUploadFile', [ $file['error'] ]);
+        if ($file['error'] !== UPLOAD_ERR_OK) {
+            if ($file['error'] === UPLOAD_ERR_INI_SIZE ||
+                    $file['error'] === UPLOAD_ERR_FORM_SIZE) {
+                $error = EC\HText::_('Upload:Errors_FileTooBig');
                 return false;
             }
 
-            if (array_key_exists('exts', $info)) {
-                $path_info = pathinfo($file['name']);
-                if (!in_array(mb_strtolower($path_info['extension']), $info['exts'])) {
-                    $error = EC\HText::_('Upload:Errors_WrongExtension');
-                    return false;
-                }
+            $error = EC\HText::_('Upload:Errors_CannotUploadFile', [ $file['error'] ]);
+            return false;
+        }
+
+        if (array_key_exists('exts', $info)) {
+            $path_info = pathinfo($file['name']);
+            if (!in_array(mb_strtolower($path_info['extension']), $info['exts'])) {
+                $error = EC\HText::_('Upload:Errors_WrongExtension');
+                return false;
             }
         }
 
